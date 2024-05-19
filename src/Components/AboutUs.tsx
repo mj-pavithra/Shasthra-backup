604b9ce1216ce50a737e1a668d8b82071757e822
mjpavithra <mjpavithra6520@gmail.com>
2024-05-19 17:45:35 +0530

    bug on about me is fixed

--- src/Components/AboutUs.tsx
+++ src/Components/AboutUs.tsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import '../Styles/AboutUs.css';
 
 const AboutUs: React.FC = () => {
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [aboutUsHeight, setAboutUsHeight] = useState<string | number>('auto');
+  // const videoRef = useRef<HTMLVideoElement>(null);
+  // const [aboutUsHeight, setAboutUsHeight] = useState<string | number>('auto');
 
-  useEffect(() => {
-    const handleVideoLoaded = () => {
-      if (videoRef.current) {
-        setAboutUsHeight(videoRef.current.videoHeight + 50);
-      }
-    };
+  // useEffect(() => {
+  //   const handleVideoLoaded = () => {
+  //     if (videoRef.current) {
+  //       setAboutUsHeight(videoRef.current.videoHeight + 50);
+  //     }
+  //   };
 
-    const videoElement = videoRef.current;
-    if (videoElement) {
-      videoElement.addEventListener('loadedmetadata', handleVideoLoaded);
-    }
+  //   const videoElement = videoRef.current;
+  //   if (videoElement) {
+  //     videoElement.addEventListener('loadedmetadata', handleVideoLoaded);
+  //   }
 
-    return () => {
-      if (videoElement) {
-        videoElement.removeEventListener('loadedmetadata', handleVideoLoaded);
-      }
-    };
-  }, []);
+  //   return () => {
+  //     if (videoElement) {
+  //       videoElement.removeEventListener('loadedmetadata', handleVideoLoaded);
+  //     }
+  //   };
+  // }, []);
 
   return (
     <div className="aboutUs" >
-      <video className="backgroundVideo" ref={videoRef} autoPlay loop muted>
+      <video className="backgroundVideo"  autoPlay loop muted>
         <source src="/introVideo.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
