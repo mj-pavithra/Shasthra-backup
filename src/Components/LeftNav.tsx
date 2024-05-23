5d9545469d34de84d4e4aa48dbead35cad33b805
mjpavithra <mjpavithra6520@gmail.com>
2024-05-23 10:58:21 +0530

    favicon and logo is added

--- src/Components/LeftNav.tsx
+++ src/Components/LeftNav.tsx
@@ -4,7 +4,7 @@ import '../Styles/LeftNav.css';
 const LeftNav:React.FC = () =>{
     return (
         <div className="left-nav">
-        <img src="../favicon2.ico" alt="logo" className="logo" />
+        <img src="../PageLogo.png" alt="logo" className="logo" />
             <h1 className="site-title">Shasthra</h1>
         </div>
     );
