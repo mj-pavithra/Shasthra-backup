d5c862fb6bf32aa684fbd4988b3a9b911b1200ff
mjpavithra <mjpavithra6520@gmail.com>
2024-06-14 12:52:16 +0530

    signup button is fixed

--- src/Components/RightNav.tsx
+++ src/Components/RightNav.tsx
@@ -11,8 +11,8 @@ const RightNav:React.FC = () =>{
 
     return (
         <div className="right-nav">
-            <h1 className="nav-button sign-in " onClick={LoadSignIn} >Sign In</h1>
-            <h1 className="nav-button sign-up" onClick={LoadSignUp}>Sign Up</h1>
+            <h1 className="nav-button-1 sign-in " onClick={LoadSignIn} >Sign In</h1>
+            <h1 className="nav-button-1 sign-up" onClick={LoadSignUp}>Sign Up</h1>
         </div>
     );
 }
